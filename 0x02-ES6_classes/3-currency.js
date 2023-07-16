fa0991c071ce7eae8e02365e7bc229a021459130
NatePillay <nathan94pillay@gmail.com>
2023-07-16 10:19:04 +0000

    new commit

---
+++ 0x02-ES6_classes/3-currency.js
@@ -0,0 +1,32 @@
+export default class Currency {
+  constructor(code, name) {
+    if (typeof code !== 'string') throw TypeError ('Code must be a string');
+    if (typeof name !== 'string') throw TypeError ('Name must be a string');
+
+    this._code = code;
+    this._name = name;
+  }
+
+
+  get code() {
+    return this._code
+  }
+
+  get name() {
+    return this._name
+  }
+
+  set name(Name) {
+    if (typeof Name !== 'string') throw TypeError('Name must be a string');
+    this._name = Name;
+  }
+
+  set code(Code) {
+    if (typeof Code !== 'string') throw TypeError('Code must be a string');
+    this._code = Code;
+  }
+
+  displayFullCurrency() {
+    return `${this._name} (${this._code})`;
+  }
+}
